231e313ae8ed368737e80ea16c427a7769215717
Joshi <sjoshi10@BANL149158403.local>
2016-03-01 19:05:30 +0530

    Copter: use mancer.js to re-write copter

--- public/app/js/copter.js
+++ public/app/js/copter.js
@@ -1,3 +1,4 @@
+
 var STEP_INTERVAL = 0.01,
     GLOBAL_X_BOUND = 800,
     GLOBAL_Y_BOUND = 400,
@@ -8,30 +9,27 @@ var STEP_INTERVAL = 0.01,
     //internal-parameters
     COPTER_COLLISION_SEARCH_WIDTH = 5
 
-var pubsub = window.pubsub;
-var Vector = function(x, y){
-    this.x = x;
-    this.y = y;
-}
-
-Vector.prototype = {
-    inc: function(point){
-        this.x += x;
-        this.y += y;
-    },
-    mag: function(){
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-    }
-}
+var pubsub = new PubSub();
 
 var G = new Vector(0, 40.83);
 
 
-var WorldObject = function(){}
-WorldObject.prototype = {
+// create our world and our objects
+var world = Object.create(World.prototype);
 
-    advanceObject: function(){
-
+//create our copter
+var copterId = world.createObject({
+    pos: new Vector(COPTER_X_POSITION,80),
+    v: new Vector(0, 300),
+    a: G,
+    draw: function(ctx){
+        ctx.beginPath();
+        ctx.fillStyle = "#fff";
+        ctx.arc(this.pos.x,this.pos.y,10,0,2*Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    },
+    tick: function(){
         // v = u + at
         this.v.x += this.a.x * STEP_INTERVAL;
         this.v.y += this.a.y * STEP_INTERVAL;
@@ -47,84 +45,16 @@ WorldObject.prototype = {
         if(this.pos.y + 10> GLOBAL_Y_BOUND ){
             this.pos.y = GLOBAL_Y_BOUND - 10;
         }
-
-        if(this.emitState && (this.emitState instanceof Function)){
-            this.emitState();
-        }
-    },
-
-    getPos: function(){
-        return new Vector(this.pos.x, this.pos.y);
-    },
-
-    setV: function(vel){
-        this.v.x = vel.x;
-        this.v.y = vel.y;
-    },
-
-    setA: function(acc){
-        this.a.x = acc.x;
-        this.a.y = acc.y;
-    }
-}
-
-// initialize World
-var World = function(){}
-World.prototype = {
-    objects: [],
-    advanceWorld: function(){
-        this.objects.forEach(function(object){
-            object.advanceObject();
-        });
-    },
-    renderWorld: function(ctx){
-        this.objects.forEach(function(object){
-            object.draw(ctx);
-        });
-    },
-    createObject: function(objectParams){
-        var worldObject = Object.create(WorldObject.prototype);
-        for(prop in objectParams){
-            if(objectParams.hasOwnProperty(prop)){
-                worldObject[prop] = objectParams[prop];
-            }
-        }
-        this.objects.push(worldObject);
-        worldObject.id = this.objects.length -1;
-        return worldObject.id;
-    },
-
-    getWorldObject: function(objectId){
-        return this.objects[objectId];
-    }
-}
-
-//create our canvas
-var canvas = document.getElementById("canvas-world");
-var ctx = canvas.getContext("2d");
-
-
-// create our world and our objects
-var world = Object.create(World.prototype);
-var copterId = world.createObject({
-    pos: new Vector(COPTER_X_POSITION,80),
-    v: new Vector(0, 300),
-    a: G,
-    draw: function(ctx){
-        ctx.beginPath();
-        ctx.fillStyle = "#fff";
-        ctx.arc(this.pos.x,this.pos.y,10,0,2*Math.PI);
-        ctx.fill();
-        ctx.closePath();
     }
 });
 
+//create block generator
 var BlockGenerator = {
     sizeParams: {
         width: 20,
         height: 120
     },
-    generateNextBlock: function(){
+    generateNextBlock: function(world){
         world.createObject({
             a: new Vector(0,0),
             v: new Vector(-(GAME_SPEED),0),
@@ -137,6 +67,23 @@ var BlockGenerator = {
                 ctx.fillStyle = "#fff";
                 ctx.fill();
                 ctx.closePath();
+            },
+            tick: function(){
+                // v = u + at
+                this.v.x += this.a.x * STEP_INTERVAL;
+                this.v.y += this.a.y * STEP_INTERVAL;
+
+
+                // s = ut + 1/2 at^2
+                this.pos.x += (this.v.x * STEP_INTERVAL + 0.5 * this.a.x * Math.pow(STEP_INTERVAL, 2));
+                this.pos.y += (this.v.y * STEP_INTERVAL + 0.5 * this.a.y * Math.pow(STEP_INTERVAL, 2));
+
+                if(this.pos.x + 10> GLOBAL_X_BOUND ){
+                    this.pos.x = GLOBAL_X_BOUND - 10;
+                }
+                if(this.pos.y + 10> GLOBAL_Y_BOUND ){
+                    this.pos.y = GLOBAL_Y_BOUND - 10;
+                }
             },
             emitState: function(){
                 if(this.pos.x < COPTER_X_POSITION + COPTER_COLLISION_SEARCH_WIDTH && this.pos.x > COPTER_X_POSITION - COPTER_COLLISION_SEARCH_WIDTH){
@@ -151,47 +98,45 @@ var BlockGenerator = {
     }
 }
 
-document.getElementsByTagName("body")[0].onkeydown = function(e){
-    if(e.keyCode == 32){
-        var copter = world.getWorldObject(copterId);
-        copter.setV(new Vector(0, -200));
-        copter.setA(new Vector(0,0));
-    }
-};
+world.init({
+    canvas: document.getElementById("canvas-world"),
+    tickSpeed: 0.01,
+    keyMap: {
+        keyup : {
+            32: function(world){
+                var copter = world.getWorldObject(copterId);
+                copter.setV(new Vector(0, 300));
+                copter.setA(G);
+            }
+        },
+        keydown: {
+            32: function(world){
+                var copter = world.getWorldObject(copterId);
+                copter.setV(new Vector(0, -200));
+                copter.setA(new Vector(0,0));
+            }
+        }
+    },
+    jobs: {
 
-document.getElementsByTagName("body")[0].onkeyup = function(e){
-    if(e.keyCode == 32){
-
-        var copter = world.getWorldObject(copterId);
-        copter.setV(new Vector(0, 300));
-        copter.setA(G);
+        blockGenerator:{
+            name: "blockGenerator",
+            worker: function(world){
+                BlockGenerator.generateNextBlock(world);
+            },
+            interval: BLOCK_GENERATION_INTERVAL,
+            from: 0
+        }
     }
-};
+
+});
 
 pubsub.sub("block-entered-collision-area", function(block){
     console.log(block.id, block.pos.x, block.pos.y, block.width, block.height);
 });
 
-pubsub.sub("block-exited-viewport", function(blockData){
+pubsub.sub("block-exited-viewport", function(block){
     console.log(block.id, block.pos.x, block.pos.y, block.width, block.height);
 });
 
-
-//start the world
-var gameLoopHandler = setInterval(function(){
-
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-
-    world.advanceWorld();
-    world.renderWorld(ctx);
-
-}, STEP_INTERVAL*1000);
-
-var blockGenerationHandler = setInterval(function(){
-    BlockGenerator.generateNextBlock();
-}, BLOCK_GENERATION_INTERVAL);
-
-
-// setTimeout(function(){
-//     clearInterval(intervalHandler);
-// }, 10000);
+world.start();
